6ecaf2bc8e7bd58e70a74d9a9ac89cc0d62e2c32
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-04-25 21:41:16 +0300

    Task 02_2

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,11 +1,19 @@
-﻿int a = 25;
-int b = 5;
-int num = b * b;
-if (num == a)
-{
-    Console.WriteLine("Условие выполнено");
-}
-else
-{
-    Console.WriteLine("Условие не выполнено");
-}
+﻿Console.Write("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+bool result = num2 * num2 == num1;
+Console.WriteLine(result ? "ДА" : "НЕТ");
+
+
+//int a = 25;
+//int b = 5;
+//int num = b * b;
+//if (num == a)
+//{
+    //Console.WriteLine("Условие выполнено");
+//}
+//else
+//{
+  //  Console.WriteLine("Условие не выполнено");
+//}
---
+++ Task_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число 3 или 5");
+string number = Console.ReadLine();
+if (number == 3)
+{
+    Console.WriteLine ("среда");
+}
+if (number == 5)
+{
+    Console.WriteLine ("пятница");
+}
+
+
+Console.WriteLine("Hello, World!");
