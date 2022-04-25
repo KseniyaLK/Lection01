986358aee3b771a7fb61165fdd2e8ee1c755f87f
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-04-25 20:57:41 +0300

    Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,8 @@
+﻿// нахождение квадрата числа
+
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен: ");
+Console.WriteLine(numResult);
+Console.ReadKey();
